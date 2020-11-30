e52139aebdea6741244fbd9c9849db2bffdb4510
asarama <arshameslami@hotmail.com>
2020-11-29 19:04:53 -0500

    Added initial work on actuator class

--- app.py
+++ app.py
@@ -1,5 +1,41 @@
+# Might need to use:
+# https://pypi.org/project/RPi.GPIO/
+import gpiozero
+
 import config
 
-print(f"{config.something} is in the config")
+class Actuator():
 
-print("Hello")
+    def __init__(self, kind, gpio_pin):
+
+        # TODO: Add if unit is wired or not
+        # Wired
+
+        # String "blower" or "heater"
+        self.kind = kind
+
+        # Boolean
+        self.active = False
+
+        # Mixed
+        self.gpio_pin = gpio_pin
+
+        self.led_instance = gpiozero.LED(self.gpio_pin)
+        self.deactivate()
+
+    def identifier_string(self):
+        return f"{self.kind} @ pin{self.gpio_pin}: "
+
+    def activate(self):
+        self.active = True
+        self.led_instance.on()
+        print(f"{self.identifier_string()}Activated")
+
+    def deactivate(self):
+        self.active = False
+        self.led_instance.off()
+        print(f"{self.identifier_string()}Deactivated")
+
+
+test_instance = Actuator("heater", 17)
+test_instance.activate()
